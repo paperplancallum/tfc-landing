a60aaad77a07346c29ca7b0b19ccaf3f4295cb7a
paperplancallum <105408952+paperplancallum@users.noreply.github.com>
2025-07-29 00:41:46 +1000

    Clean up debug files and test pages

--- app/simple-test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SimpleTestPage() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Simple Test Page</h1>
-      <p>If you can see this, routing is working.</p>
-    </div>
-  )
-}
